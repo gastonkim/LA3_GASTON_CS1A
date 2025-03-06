38b6eb8cfb110e679fcd4fe8bcc8d1dd5dea01e0
gastonkim <gastonkimcarlos@gmail.com>
2025-03-06 19:50:17 +0800

    Add files via upload

---
+++ activity5.js
@@ -0,0 +1,18 @@
+// Declaring string variables
+let favActorFirstName = "Keanu";
+let favActorLastName = "Reeves";
+
+// Concatenating first and last name
+let fullName = favActorFirstName + " " + favActorLastName;
+
+// Converting to uppercase
+let uppercase = fullName.toUpperCase();
+
+// Creating message
+let message = "My favorite actor is " + uppercase + ".";
+message += " His best show is Silicon Valley.";
+
+// Logging variables
+console.log("Full Name:", fullName);
+console.log("Uppercase Name:", uppercase);
+console.log("Message:", message);
