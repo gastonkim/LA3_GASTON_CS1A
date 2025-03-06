7944313d78d46de5b2777fb58a833c75d200c4d0
gastonkim <gastonkimcarlos@gmail.com>
2025-03-06 19:49:39 +0800

    Add files via upload

---
+++ actvity3.js
@@ -0,0 +1,12 @@
+// Declaring boolean variables
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 7;
+
+// Adding undefined to favNumb
+favNumb = undefined;
+
+// Logging variables
+console.log("Is JavaScript a programming language?", isJsProgrammingLanguage);
+console.log("Is JavaScript hard?", isJsHard);
+console.log("Favorite Number:", favNumb);
