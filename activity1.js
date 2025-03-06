ccfefc692eafaf21050c2e6b5f5bffafdc4063d1
gastonkim <gastonkimcarlos@gmail.com>
2025-03-06 19:49:00 +0800

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,11 @@
+// Declaring variables
+let name = "Kim Carlos Gaston";
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Male";
+let twitterHandle = "@kimcarlos";
+
+// Logging variables
+console.log("Name:", name);
+console.log("Dream Job:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
