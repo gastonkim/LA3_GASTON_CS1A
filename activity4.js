5f1751bfb58c8883f858fe425540b8951ba5954c
gastonkim <gastonkimcarlos@gmail.com>
2025-03-06 19:49:53 +0800

    Add files via upload

---
+++ activity4.js
@@ -0,0 +1,13 @@
+// Declaring number variables
+let firstFavNumb = 7;
+let secondFavNumb = 3;
+
+// Performing comparisons
+console.log("Greater than:", firstFavNumb > secondFavNumb);
+console.log("Less than:", firstFavNumb < secondFavNumb);
+console.log("Greater than or equal to:", firstFavNumb >= secondFavNumb);
+console.log("Less than or equal to:", firstFavNumb <= secondFavNumb);
+console.log("Strict equality:", firstFavNumb === secondFavNumb);
+console.log("Loose equality:", firstFavNumb == secondFavNumb);
+console.log("Strict inequality:", firstFavNumb !== secondFavNumb);
+console.log("Loose inequality:", firstFavNumb != secondFavNumb);
